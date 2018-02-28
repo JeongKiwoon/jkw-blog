9f0ddd098772bfa02b5ee38b30d7316d72ba959a
JeongKiwoon <jkw9404@gmail.com>
2018-02-28 18:24:23 +0900

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,36 @@
 
   $(function () {
     // 전역변수
+
+    $('html').removeClass('no-js');
+
     var
         $wl_item = $('.wl-item'),
-        $wl_image = $('.wl-image');
+        $wl_image = $('.wl-image'),
+
+        mql = window.matchMedia('screen and (max-width: 959px)');
 
     // Connect Event
     $wl_item.on('mouseenter', showWorkImage);
     $wl_item.on('mouseleave', hideWorkImage);
+
+    if (mql.matches) {
+      console.log('처음에 모바일로 접속했으니까 이 이벤트를 삭제합니다.');
+      $wl_item.off('mouseenter');
+      $wl_item.off('mouseleave');
+    }
+
+    mql.addListener(function(e) {
+      if (e.matches) {
+	        console.log('work-image가 사라집니다.');
+          $wl_item.off('mouseenter');
+          $wl_item.off('mouseleave');
+	    } else {
+	        console.log('work-image가 나타나있습니다.');
+          $wl_item.on('mouseenter', showWorkImage);
+          $wl_item.on('mouseleave', hideWorkImage);
+	    }
+    });
 
     // Event Handler
     function showWorkImage(e) {
